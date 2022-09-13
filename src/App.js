1f4cb11d2894789d6e9b8008dd6e4ecfd6f8cfd6
GWNR71517\joshu <joshuabliven93@gmail.com>
2022-09-13 15:50:51 -0400

    save cart items to local storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Basket from './components/Basket';
 import data from './data';
+
+function Save(newCartItems) {
+	const savedCartItems = newCartItems;
+	localStorage.setItem('cartItems', JSON.stringify(savedCartItems));
+}
 
 function App() {
     const [cartItems, setCartItems] = useState([]);
@@ -15,9 +20,11 @@ function App() {
             );
 
             setCartItems(newCartItems);
+            Save(newCartItems);
         } else {
             const newCartItems = [...cartItems, {...product, qty: 1}];
             setCartItems(newCartItems);
+            Save(newCartItems);
         }
     }
     const onRemove = (product) => {
@@ -25,13 +32,22 @@ function App() {
         if (exist.qty === 1) {
             const newCartItems = cartItems.filter((x) => x.id !== product.id);
             setCartItems(newCartItems)
+            Save(newCartItems);
         } else {
             const newCartItems = cartItems.map((x) =>
                 x.id === product.id ? {...exist, qty: exist.qty - 1} : x
             );
             setCartItems(newCartItems);
+            Save(newCartItems);
         }
     };
+
+    useEffect(() => {
+    	setCartItems(localStorage.getItem('cartItems')
+    		? JSON.parse(localStorage.getItem('cartItems'))
+    		: []
+    	);
+    }, []);
     return (
         <div>
             <Header countCartItems={cartItems.reduce((acc, i) => acc + i.qty, 0)} />
